ec0ae4b0d75b1e22e54e9b58d35e7f1790c14f5f
virgile <boulanger.virgile@gmail.com>
2024-06-13 10:13:31 +0200

    chore: modify torchscript io dims

--- models/shortres_msd/model.ts
+++ models/shortres_msd/model.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c913523e90b25a0c52d0b09d23990a57f39bb779e69c25275ed4acb380dcc45
-size 48705076
+oid sha256:dcf8c0358bf5e3c73e10a58a0b439ea10e1ed819428b7124e88caef04d59e263
+size 48768673
