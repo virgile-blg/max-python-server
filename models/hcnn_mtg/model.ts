910288ffb2082f9915b4c05c165826082c24fb81
virgile <boulanger.virgile@gmail.com>
2024-06-13 10:14:00 +0200

    feat: add new ts models

---
+++ models/hcnn_mtg/model.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f91ec4d0bfa777cddb1e566eca88b0603974423e5e80718502bbb6edbf4f1185
+size 14683683
---
+++ models/shortres_mtg/model.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a69e70b3c725c0f66a311e7a04fcf2f11c29b83730e6a464e801b3dbafbc384a
+size 48761093
